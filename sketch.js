a7b4f829f53fdd83cd54fb29a077d5c9a841186b
Morgan Lee <mhlee4@dons.usfca.edu>
2022-01-27 15:24:05 -0800

    first commit

---
+++ sketch.js
@@ -0,0 +1,36 @@
+
+var x = 500;
+var y = 500;
+var xJump = 15;
+var yJump = 15;
+var shade = '#FFFFF'
+
+// Setup code goes here
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+
+  textSize(50);
+  textAlign(CENTER);
+
+ }
+
+
+// Draw code goes here
+function draw() {
+  background('#9D5C63');
+
+  fill('#584b53');
+  text("Hello, World.", width/2, height/2);
+
+  fill(shade);
+  noStroke();
+  circle(x, y, 100);
+  x += xJump;
+  if(x > 1000 || x < 100){
+    xJump *= -1;
+  }
+
+  if(mouseX == width/2 || mouseY == height/2){
+    xJump = 0;
+  }
+}
